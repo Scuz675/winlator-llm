991d16f9c0530d672cb146ffbe0c7337a91b0bcf
Scuz675 <curveuk@gmail.com>
2026-08-17 20:02:56 +0100

    Top aligned render for fold screens

--- app/src/main/java/com/winlator/renderer/ViewTransformation.java
+++ app/src/main/java/com/winlator/renderer/ViewTransformation.java
@@ -16,11 +16,11 @@ public class ViewTransformation {
         viewWidth = (int)Math.ceil(innerWidth * aspect);
         viewHeight = (int)Math.ceil(innerHeight * aspect);
         viewOffsetX = (int)((outerWidth - innerWidth * aspect) * 0.5f);
-        viewOffsetY = (int)((outerHeight - innerHeight * aspect) * 0.5f);
+        viewOffsetY = outerHeight - viewHeight;
 
         sceneScaleX = (innerWidth * aspect) / outerWidth;
         sceneScaleY = (innerHeight * aspect) / outerHeight;
         sceneOffsetX = (innerWidth - innerWidth * sceneScaleX) * 0.5f;
-        sceneOffsetY = (innerHeight - innerHeight * sceneScaleY) * 0.5f;
+        sceneOffsetY = 0;
     }
 }
